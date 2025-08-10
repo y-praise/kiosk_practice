4b465971646181f841ab57365f1543f59d8ab4fb
hajjun <81499812+hajjun@users.noreply.github.com>
2025-08-10 20:23:45 +0900

    Delete lib/table_lib/models directory

--- lib/table_lib/models/challenge.dart
+++
@@ -1,7 +0,0 @@
-class Challenge {
-  final String description; // Challenge 설명
-  // Challenge에 필요한 아이템의 ID와 개수를 나타내는 맵
-  final Map<int, int> requiredItems; // 아이템 ID와 개수
-
-  Challenge({required this.description, required this.requiredItems});
-}
