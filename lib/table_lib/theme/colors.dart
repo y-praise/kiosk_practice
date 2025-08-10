a24bd7adf1ec3981140e46d5996c2ab343b806a9
hajjun <81499812+hajjun@users.noreply.github.com>
2025-08-10 20:24:02 +0900

    Delete lib/table_lib/theme directory

--- lib/table_lib/theme/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// 앱의 주요 색상 정의
-const Color primaryColor = Color(0xFF4A6572); //  네이비
-const Color accentColor = Color(0xFFF9AA33); //  옐로우
-const Color backgroundColor = Color(0xFFF9F9F9); // 배경색
-const Color textColor = Color(0xFF333333); // 기본 텍스트 색상
-const Color subTextColor = Color(0xFF757575); // 보조 텍스트 색상
